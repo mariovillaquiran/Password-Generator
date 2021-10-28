af510874c962b61f4f0bc464f9ea051a07885da5
Mario Villaquiran <mariovillaquiran@Marios-Air.localdomain>
2021-10-28 19:57:56 -0400

    Worked on variables

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 // Assignment code here
 
+//Add special chatacters
+var specialcharacter = ["!", "@","etc"]
+var numberscharacters = ["1", "2". etc]
+var lowercasecharacters = ["a" etc...]
+var uppercasecharacters = ["A" etc...]
+
+function passwordoptions () {
+var characterlength = parseInt(
+  prompt("How long would you like your password to be?")
+)
+if (Number.isNaN(characterlength)) {
+  alert ("Try Again")
+  return null
+}
+if (characterlength <8) {
+  alert ("Try Again")
+  return null
+}
+if (characterlegnth >128) {
+  alert ("try again")
+  return null 
+}
+// add fpr the rest of the variables (see above)
+var specialcharacterchoicelist = confirm(
+  "Click Ok to include your special characters"
+)
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
